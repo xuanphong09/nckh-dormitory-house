29d9b4671c995322934f7ed08593e46391fdcb4b
Nguyễn Phương Nam <namdev@mac.lan>
2025-10-10 16:56:06 +0700

    install bootstrap

---
+++ resources/views/layout_test.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Layout Bootstrap</title>
+    @vite('resources/js/app.js')
+</head>
+<body>
+    <div class="container">
+        <div class="mb-3">
+            <label for="exampleFormControlInput1" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+        </div>
+        <div class="mb-3">
+            <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+        </div> 
+    </div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    return view('layout_test');
 });
