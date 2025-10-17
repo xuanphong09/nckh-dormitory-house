aed02e2b4e138385b50162a7f387edf26bc1226a
Danh0706 <nicebody2709@gmail.com>
2025-10-17 15:35:01 +0700

    tesst

--- routes/web.php
+++ routes/web.php
@@ -12,4 +12,4 @@ Route::get('/feedback/create', [FeedbackController::class, 'create']);
 Route::post('/feedback/store', [FeedbackController::class, 'store']);
 Route::get('/feedback/pending', [FeedbackController::class, 'listPending']);
 Route::post('/feedback/process/{id}', [FeedbackController::class, 'process']);
-//Route::get('/feedback/all', [FeedbackController::class, 'listAll']);
+Route::get('/feedback/all', [FeedbackController::class, 'listAll']);
